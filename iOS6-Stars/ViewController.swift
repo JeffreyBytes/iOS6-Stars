080dd0d7f7d27c5a21346afd5b275854061a7f64
Jeffrey Carpenter <spaz926@me.com>
2019-04-30 11:11:23 -0500

    Set up initial Xcode project

---
+++ iOS6-Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS6-Stars
+//
+//  Created by Jeffrey Carpenter on 4/30/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
